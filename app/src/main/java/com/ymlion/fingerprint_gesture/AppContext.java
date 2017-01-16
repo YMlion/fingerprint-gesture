6a1b292a9660c366aab4b36ee476bd079e7079d4
YMlion <lm_yao@163.com>
2017-01-16 22:10:36 +0800

    init

---
+++ app/src/main/java/com/ymlion/fingerprint_gesture/AppContext.java
@@ -0,0 +1,20 @@
+package com.ymlion.fingerprint_gesture;
+
+import android.app.Application;
+
+/**
+ * Created by ymlion on 2017/1/16
+ */
+
+public class AppContext extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
